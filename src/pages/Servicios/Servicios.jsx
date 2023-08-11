edf4c982552fe209c775541219887dc9d7ca2208
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2023-08-10 18:47:48 -0600

    6. Arreglados los estilos de los tabs de servicios y cambiados por el componente de angular react

--- src/pages/Servicios/Servicios.jsx
+++ src/pages/Servicios/Servicios.jsx
@@ -1,5 +1,9 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import ListGroup from "react-bootstrap/ListGroup";
+import Tab from "react-bootstrap/Tab";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Servicios() {
 	return (
@@ -15,222 +19,207 @@ function Servicios() {
 					</div>
 				</div>
 			</div>
-
 			<div className="single-services">
 				<div className="container">
-					<div className="row" id="tabs">
-						<div className="col-md-4">
-							<ul>
-								<li>
-									<a href="#tabs-1">
-										Traslados<i className="fa fa-angle-right"></i>
-									</a>
-								</li>
-								<li>
-									<a href="#tabs-2">
+					<Tab.Container defaultActiveKey="#link1">
+						<Row id="tabs">
+							<Col sm={4}>
+								<ListGroup>
+									<ListGroup.Item action href="#link1">
+										Traslados <i className="fa fa-angle-right"></i>
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link2">
 										Capilla a domicilio <i className="fa fa-angle-right"></i>
-									</a>
-								</li>
-								<li>
-									<a href="#tabs-3">
-										Ceremonia religiosa <i className="fa fa-angle-right"></i>
-									</a>
-								</li>
-								<li>
-									<a href="#tabs-4">
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link3">
+										Ceremonioa religiosa <i className="fa fa-angle-right"></i>
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link4">
 										Homenaje en video <i className="fa fa-angle-right"></i>
-									</a>
-								</li>
-								<li>
-									<a href="#tabs-5">
-										Condolencia digital* <i className="fa fa-angle-right"></i>
-									</a>
-								</li>
-								<li>
-									<a href="#tabs-6">
-										Funerales virtuales <i className="fa fa-angle-right"></i>
-									</a>
-								</li>
-								<li>
-									<a href="#tabs-7">
-										Autobús de acompañamiento**{" "}
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link5">
+										Condolencia digital*
 										<i className="fa fa-angle-right"></i>
-									</a>
-								</li>
-								<li>
-									<a href="#tabs-8">
-										Embalsamamiento <i className="fa fa-angle-right"></i>
-									</a>
-								</li>
-							</ul>
-						</div>
-						<div className="col-md-8">
-							<section className="tabs-content">
-								<article id="tabs-1">
-									<img src="assets/images/servicio_01.png" alt="" />
-									<h4>Traslados</h4>
-									<p>
-										Vivamus sed feugiat elit. Pellentesque pretium, massa at
-										placerat vehicula, neque turpis pulvinar tortor, eget
-										convallis lorem odio non tortor. Donec massa est, fermentum
-										sit amet felis ac, maximus luctus elit. Vivamus aliquet,
-										dolor id imperdiet imperdiet, dui diam aliquet dui, a
-										euismod metus enim ac velit. Vivamus eu tristique odio, vel
-										tristique quam.
-										<br />
-										<br />
-										Proin eu molestie risus. Etiam suscipit pretium odio, at
-										consectetur nisi. Sed ut dolor in augue cursus ultrices.
-										Vivamus mauris turpis, auctor vel facilisis in, tincidunt
-										vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
-										Aliquam commodo mauris ante.
-									</p>
-								</article>
-								<article id="tabs-2">
-									<img src="assets/images/servicio_02.png" alt="" />
-									<h4>Capilla a domicilio</h4>
-									<p>
-										Sed ut dolor in augue cursus ultrices. Vivamus mauris
-										turpis, auctor vel facilisis in, tincidunt vel diam. Sed
-										vitae scelerisque orci. Nunc non magna orci. Aliquam commodo
-										mauris ante, quis posuere nibh vestibulum sit amet
-										<br />
-										<br />
-										Pellentesque pretium, massa at placerat vehicula, neque
-										turpis pulvinar tortor, eget convallis lorem odio non
-										tortor. Donec massa est, fermentum sit amet felis ac,
-										maximus luctus elit. Vivamus aliquet, dolor id imperdiet
-										imperdiet, dui diam aliquet dui, a euismod metus enim ac
-										velit. Vivamus eu tristique odio, vel tristique quam.
-									</p>
-								</article>
-								<article id="tabs-3">
-									<img src="assets/images/servicio_03.png" alt="" />
-									<h4>Ceremonia religiosa</h4>
-									<p>
-										Mauris lobortis quam id dictum dignissim. Donec pellentesque
-										erat dolor, cursus dapibus turpis hendrerit quis.
-										Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla
-										facilisi. In sit amet neque sapien. Donec scelerisque mi at
-										gravida efficitur. Nunc lacinia a est eu malesuada.
-										Curabitur eleifend elit sapien, sed pulvinar orci luctus
-										eget. <br />
-										<br />
-										Class aptent taciti sociosqu ad litora torquent per conubia
-										nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac
-										tincidunt eros. Aenean quis tellus velit. Praesent pretium
-										justo non auctor condimentum.
-									</p>
-								</article>
-								<article id="tabs-4">
-									<img src="assets/images/servicio_04.png" alt="" />
-									<h4>Homenaje en video</h4>
-									<p>
-										Integer vehicula sapien quis dolor efficitur, eget molestie
-										eros tempus. Curabitur sollicitudin, tortor at suscipit
-										volutpat, nisi arcu aliquet dui, vitae semper sem turpis
-										quis libero. Quisque vulputate lacinia nisl ac lobortis. Ut
-										ultricies maximus turpis, in sollicitudin ligula posuere
-										vel.
-										<br />
-										<br />
-										Donec finibus maximus neque, vitae egestas quam imperdiet
-										nec. Proin nec mauris eu tortor consectetur tristique. Etiam
-										suscipit ante a odio consequat, in ornare lacus suscipit.
-										Cras ac libero massa. Quisque rhoncus velit feugiat
-										vulputate mollis. Donec nisl eros, malesuada sed nisi id,
-										condimentum condimentum quam.
-									</p>
-								</article>
-								<article id="tabs-5">
-									<img src="assets/images/servicio_05.png" alt="" />
-									<h4>Condolencia digital*</h4>
-									<p>
-										Vivamus sed feugiat elit. Pellentesque pretium, massa at
-										placerat vehicula, neque turpis pulvinar tortor, eget
-										convallis lorem odio non tortor. Donec massa est, fermentum
-										sit amet felis ac, maximus luctus elit. Vivamus aliquet,
-										dolor id imperdiet imperdiet, dui diam aliquet dui, a
-										euismod metus enim ac velit. Vivamus eu tristique odio, vel
-										tristique quam.
-										<br />
-										<br />
-										Proin eu molestie risus. Etiam suscipit pretium odio, at
-										consectetur nisi. Sed ut dolor in augue cursus ultrices.
-										Vivamus mauris turpis, auctor vel facilisis in, tincidunt
-										vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
-										Aliquam commodo mauris ante.
-									</p>
-									<br />
-									<br />
-									<br />
-									<br />
-									<p>*Exclusivo en Agencia Funeraria Naser Morelos</p>
-								</article>
-								<article id="tabs-6">
-									<img src="assets/images/servicio_06.png" alt="" />
-									<h4>Funerales virtuales</h4>
-									<p>
-										Sed ut dolor in augue cursus ultrices. Vivamus mauris
-										turpis, auctor vel facilisis in, tincidunt vel diam. Sed
-										vitae scelerisque orci. Nunc non magna orci. Aliquam commodo
-										mauris ante, quis posuere nibh vestibulum sit amet
-										<br />
-										<br />
-										Pellentesque pretium, massa at placerat vehicula, neque
-										turpis pulvinar tortor, eget convallis lorem odio non
-										tortor. Donec massa est, fermentum sit amet felis ac,
-										maximus luctus elit. Vivamus aliquet, dolor id imperdiet
-										imperdiet, dui diam aliquet dui, a euismod metus enim ac
-										velit. Vivamus eu tristique odio, vel tristique quam.
-									</p>
-								</article>
-								<article id="tabs-7">
-									<img src="assets/images/servicio_07.png" alt="" />
-									<h4>Autobús de acompañamiento</h4>
-									<p>
-										Mauris lobortis quam id dictum dignissim. Donec pellentesque
-										erat dolor, cursus dapibus turpis hendrerit quis.
-										Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla
-										facilisi. In sit amet neque sapien. Donec scelerisque mi at
-										gravida efficitur. Nunc lacinia a est eu malesuada.
-										Curabitur eleifend elit sapien, sed pulvinar orci luctus
-										eget. <br />
-										<br />
-										Class aptent taciti sociosqu ad litora torquent per conubia
-										nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac
-										tincidunt eros. Aenean quis tellus velit. Praesent pretium
-										justo non auctor condimentum.
-									</p>
-									<br />
-									<br />
-									<br />
-									<br />
-									<p>*Exclusivo en Agencia Funeraria Naser Aragón</p>
-								</article>
-								<article id="tabs-8">
-									<img src="assets/images/servicio_08.png" alt="" />
-									<h4>Embalsamamiento</h4>
-									<p>
-										Integer vehicula sapien quis dolor efficitur, eget molestie
-										eros tempus. Curabitur sollicitudin, tortor at suscipit
-										volutpat, nisi arcu aliquet dui, vitae semper sem turpis
-										quis libero. Quisque vulputate lacinia nisl ac lobortis. Ut
-										ultricies maximus turpis, in sollicitudin ligula posuere
-										vel.
-										<br />
-										<br />
-										Donec finibus maximus neque, vitae egestas quam imperdiet
-										nec. Proin nec mauris eu tortor consectetur tristique. Etiam
-										suscipit ante a odio consequat, in ornare lacus suscipit.
-										Cras ac libero massa. Quisque rhoncus velit feugiat
-										vulputate mollis. Donec nisl eros, malesuada sed nisi id,
-										condimentum condimentum quam.
-									</p>
-								</article>
-							</section>
-						</div>
-					</div>
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link6">
+										Funerales virtuales <i className="fa fa-angle-right"></i>
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link7">
+										Autobus de acompañamiento**
+										<i className="fa fa-angle-right"></i>
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link8">
+										Embalsamamiento
+										<i className="fa fa-angle-right"></i>
+									</ListGroup.Item>
+								</ListGroup>
+							</Col>
+							<Col sm={8}>
+								<Tab.Content>
+									<Tab.Pane eventKey="#link1">
+										<img src="assets/images/servicio_01.png" alt="" />
+										<h4>Traslados</h4>
+										<p>
+											Vivamus sed feugiat elit. Pellentesque pretium, massa at
+											placerat vehicula, neque turpis pulvinar tortor, eget
+											convallis lorem odio non tortor. Donec massa est,
+											fermentum sit amet felis ac, maximus luctus elit. Vivamus
+											aliquet, dolor id imperdiet imperdiet, dui diam aliquet
+											dui, a euismod metus enim ac velit. Vivamus eu tristique
+											odio, vel tristique quam.
+											<br />
+											<br />
+											Proin eu molestie risus. Etiam suscipit pretium odio, at
+											consectetur nisi. Sed ut dolor in augue cursus ultrices.
+											Vivamus mauris turpis, auctor vel facilisis in, tincidunt
+											vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
+											Aliquam commodo mauris ante.
+										</p>
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link2">
+										<img src="assets/images/servicio_01.png" alt="" />
+										<h4>Capilla a domicilio</h4>
+										<p>
+											Vivamus sed feugiat elit. Pellentesque pretium, massa at
+											placerat vehicula, neque turpis pulvinar tortor, eget
+											convallis lorem odio non tortor. Donec massa est,
+											fermentum sit amet felis ac, maximus luctus elit. Vivamus
+											aliquet, dolor id imperdiet imperdiet, dui diam aliquet
+											dui, a euismod metus enim ac velit. Vivamus eu tristique
+											odio, vel tristique quam.
+											<br />
+											<br />
+											Proin eu molestie risus. Etiam suscipit pretium odio, at
+											consectetur nisi. Sed ut dolor in augue cursus ultrices.
+											Vivamus mauris turpis, auctor vel facilisis in, tincidunt
+											vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
+											Aliquam commodo mauris ante.
+										</p>
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link3">
+										<img src="assets/images/servicio_01.png" alt="" />
+										<h4>Ceremonia religiosa</h4>
+										<p>
+											Vivamus sed feugiat elit. Pellentesque pretium, massa at
+											placerat vehicula, neque turpis pulvinar tortor, eget
+											convallis lorem odio non tortor. Donec massa est,
+											fermentum sit amet felis ac, maximus luctus elit. Vivamus
+											aliquet, dolor id imperdiet imperdiet, dui diam aliquet
+											dui, a euismod metus enim ac velit. Vivamus eu tristique
+											odio, vel tristique quam.
+											<br />
+											<br />
+											Proin eu molestie risus. Etiam suscipit pretium odio, at
+											consectetur nisi. Sed ut dolor in augue cursus ultrices.
+											Vivamus mauris turpis, auctor vel facilisis in, tincidunt
+											vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
+											Aliquam commodo mauris ante.
+										</p>
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link4">
+										<img src="assets/images/servicio_01.png" alt="" />
+										<h4>Homenaje en video</h4>
+										<p>
+											Vivamus sed feugiat elit. Pellentesque pretium, massa at
+											placerat vehicula, neque turpis pulvinar tortor, eget
+											convallis lorem odio non tortor. Donec massa est,
+											fermentum sit amet felis ac, maximus luctus elit. Vivamus
+											aliquet, dolor id imperdiet imperdiet, dui diam aliquet
+											dui, a euismod metus enim ac velit. Vivamus eu tristique
+											odio, vel tristique quam.
+											<br />
+											<br />
+											Proin eu molestie risus. Etiam suscipit pretium odio, at
+											consectetur nisi. Sed ut dolor in augue cursus ultrices.
+											Vivamus mauris turpis, auctor vel facilisis in, tincidunt
+											vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
+											Aliquam commodo mauris ante.
+										</p>
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link5">
+										<img src="assets/images/servicio_01.png" alt="" />
+										<h4>Condolencia digital*</h4>
+										<p>
+											Vivamus sed feugiat elit. Pellentesque pretium, massa at
+											placerat vehicula, neque turpis pulvinar tortor, eget
+											convallis lorem odio non tortor. Donec massa est,
+											fermentum sit amet felis ac, maximus luctus elit. Vivamus
+											aliquet, dolor id imperdiet imperdiet, dui diam aliquet
+											dui, a euismod metus enim ac velit. Vivamus eu tristique
+											odio, vel tristique quam.
+											<br />
+											<br />
+											Proin eu molestie risus. Etiam suscipit pretium odio, at
+											consectetur nisi. Sed ut dolor in augue cursus ultrices.
+											Vivamus mauris turpis, auctor vel facilisis in, tincidunt
+											vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
+											Aliquam commodo mauris ante.
+										</p>
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link6">
+										<img src="assets/images/servicio_01.png" alt="" />
+										<h4>Funerales virtuales</h4>
+										<p>
+											Vivamus sed feugiat elit. Pellentesque pretium, massa at
+											placerat vehicula, neque turpis pulvinar tortor, eget
+											convallis lorem odio non tortor. Donec massa est,
+											fermentum sit amet felis ac, maximus luctus elit. Vivamus
+											aliquet, dolor id imperdiet imperdiet, dui diam aliquet
+											dui, a euismod metus enim ac velit. Vivamus eu tristique
+											odio, vel tristique quam.
+											<br />
+											<br />
+											Proin eu molestie risus. Etiam suscipit pretium odio, at
+											consectetur nisi. Sed ut dolor in augue cursus ultrices.
+											Vivamus mauris turpis, auctor vel facilisis in, tincidunt
+											vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
+											Aliquam commodo mauris ante.
+										</p>
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link7">
+										<img src="assets/images/servicio_01.png" alt="" />
+										<h4>Autobús de acompañamiento</h4>
+										<p>
+											Vivamus sed feugiat elit. Pellentesque pretium, massa at
+											placerat vehicula, neque turpis pulvinar tortor, eget
+											convallis lorem odio non tortor. Donec massa est,
+											fermentum sit amet felis ac, maximus luctus elit. Vivamus
+											aliquet, dolor id imperdiet imperdiet, dui diam aliquet
+											dui, a euismod metus enim ac velit. Vivamus eu tristique
+											odio, vel tristique quam.
+											<br />
+											<br />
+											Proin eu molestie risus. Etiam suscipit pretium odio, at
+											consectetur nisi. Sed ut dolor in augue cursus ultrices.
+											Vivamus mauris turpis, auctor vel facilisis in, tincidunt
+											vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
+											Aliquam commodo mauris ante.
+										</p>
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link8">
+										<img src="assets/images/servicio_01.png" alt="" />
+										<h4>Embalsamamiento</h4>
+										<p>
+											Vivamus sed feugiat elit. Pellentesque pretium, massa at
+											placerat vehicula, neque turpis pulvinar tortor, eget
+											convallis lorem odio non tortor. Donec massa est,
+											fermentum sit amet felis ac, maximus luctus elit. Vivamus
+											aliquet, dolor id imperdiet imperdiet, dui diam aliquet
+											dui, a euismod metus enim ac velit. Vivamus eu tristique
+											odio, vel tristique quam.
+											<br />
+											<br />
+											Proin eu molestie risus. Etiam suscipit pretium odio, at
+											consectetur nisi. Sed ut dolor in augue cursus ultrices.
+											Vivamus mauris turpis, auctor vel facilisis in, tincidunt
+											vel diam. Sed vitae scelerisque orci. Nunc non magna orci.
+											Aliquam commodo mauris ante.
+										</p>
+									</Tab.Pane>
+								</Tab.Content>
+							</Col>
+						</Row>
+					</Tab.Container>
 				</div>
 			</div>
 			<div className="espacio"></div>
